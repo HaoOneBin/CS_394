94289c1642a538c4a0a77bf8b06d17a09fbf2576
HaoOneBin <37146238+HaoOneBin@users.noreply.github.com>
2022-01-04 16:01:52 -0600

    Style the app with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 
 const schedule = {
   title: "CS Courses for 2018-2019",
@@ -29,7 +30,7 @@ const schedule = {
 const Banner = ({ title }) => <h1>{title}</h1>;
 
 const CourseList = ({ courses }) => (
-  <div>
+  <div className="course-list">
     {Object.values(courses).map((course) => (
       <Course key={course.id} course={course} />
     ))}
@@ -43,13 +44,18 @@ const getCourseTerm = (course) => terms[course.id.charAt(0)];
 const getCourseNumber = (course) => course.id.slice(1, 4);
 
 const Course = ({ course }) => (
-  <div>
-    {getCourseTerm(course)} CS {getCourseNumber(course)}: {course.title}
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">
+        {getCourseTerm(course)} CS {getCourseNumber(course)}
+      </div>
+      <div className="card-text">{course.title}</div>
+    </div>
   </div>
 );
 
 const App = () => (
-  <div>
+  <div className="container">
     <Banner title={schedule.title} />
     <CourseList courses={schedule.courses} />
   </div>
