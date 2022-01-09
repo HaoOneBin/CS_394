43a0926d640d273f8c815701ba32fce653f6e320
HaoOneBin <37146238+HaoOneBin@users.noreply.github.com>
2022-01-09 14:42:53 -0600

    Store data

--- src/compoents/CourseList.js
+++ src/compoents/CourseList.js
@@ -27,7 +27,7 @@ const TermButton = ({ term, setTerm, checked }) => (
       checked={checked}
       onChange={() => setTerm(term)}
     />
-    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+    <label className="btn btn-success m-1 p-2" htmlFor={term}>
       {term}
     </label>
   </>
